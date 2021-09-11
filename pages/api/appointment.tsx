dd1399c989a3f7a339afa47a9f947a17e9fb8fe1
Eric Alencar <eric.alencar@imd.ufrn.br>
2021-09-10 21:03:18 -0300

    adição das rotas de appointment, search e teacher; adição de validação de login na rota de appointment;

---
+++ pages/api/appointment.tsx
@@ -0,0 +1,115 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { NextApiRequest, NextApiResponse } from 'next';
+import { getSession } from 'next-auth/client';
+import connect from '../../utils/database';
+import { ObjectID } from 'mongodb';
+
+interface ErrorResponseType {
+  error: string;
+}
+
+interface SuccessResponseType {
+  date:string;
+  teacher_name:string;
+  teacher_id:string;
+  student_id:string;
+  student_name:string;
+  course:string;
+  location:string;
+  appointment_link:string;
+}
+
+export default async (
+  req: NextApiRequest,
+  res: NextApiResponse<ErrorResponseType | SuccessResponseType>
+): Promise<void> => {
+  if (req.method === 'POST') {
+    const session = await getSession({ req });
+
+    // verificação se o usuário está logado
+    if(!session) {
+        res.status(400).json({ error: 'Please login first!' });
+        return;
+    }
+    
+    const {
+        date,
+        teacher_name,
+        teacher_id,
+        student_id,
+        student_name,
+        course,
+        location,
+        appointment_link,
+    } = req.body;
+
+    if (
+      !date ||
+      !teacher_name ||
+      !teacher_id ||
+      !student_id ||
+      !student_name ||
+      !course ||
+      !location
+    ) {
+      res.status(400).json({ error: 'Missing parameter on request body!' });
+      return;
+    }
+    const { db } = await connect();
+
+    const teacherExists = await db
+      .collection('users')
+      .findOne({ _id: new ObjectID(teacher_id) });
+
+    if (!teacherExists) {
+      res
+        .status(400)
+        .json({
+          error: `teacher ${teacher_name} with id ${teacher_id} does not exist`,
+        });
+      return;
+    }
+
+    const studentExists = await db
+      .collection('users')
+      .findOne({ _id: new ObjectID(student_id) });
+
+    if (!studentExists) {
+      res
+        .status(400)
+        .json({
+          error: `student ${student_name} with id ${student_id} does not exist`,
+        });
+      return;
+    }
+
+    const appointment = {
+      date,
+      teacher_name,
+      teacher_id,
+      student_id,
+      student_name,
+      course,
+      location,
+      appointment_link: appointment_link || '',
+    };
+
+    await db
+      .collection('users')
+      .updateOne(
+        { _id: new ObjectID(teacher_id) },
+        { $push: { appointments: appointment } }
+      );
+
+    await db
+      .collection('users')
+      .updateOne(
+        { _id: new ObjectID(student_id) },
+        { $push: { appointments: appointment } }
+      );
+
+    res.status(200).json(appointment);
+  } else {
+    res.status(400).json({ error: 'erro ao processar a requisição' });
+  }
+};
--- pages/api/user.tsx
+++ pages/api/user.tsx
@@ -1,38 +1,94 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import { NextApiRequest, NextApiResponse } from 'next';
-import connect from '../../utils/database'
+import connect from '../../utils/database';
 
 interface ErrorResponseType {
   error: string;
 }
 
 interface SuccessResponseType {
-  _id: string,
-  name: string,
-  email: string,
-  cellphone: string,
-  teacher: boolean,
+  _id: string;
+  name: string;
+  email: string;
+  cellphone: string;
+  teacher: true;
+  coins: 1;
+  courses: string[];
+  available_hours: object;
+  available_locations: string[];
+  reviews: object[];
+  appointments: object[];
 }
 
 export default async (
-  req: NextApiRequest, 
+  req: NextApiRequest,
   res: NextApiResponse<ErrorResponseType | SuccessResponseType>
-  ): Promise<void> => {
-    if (req.method === 'POST') {
-      const {name, email, cellphone, teacher } = req.body;
-    
-      if(!name || !email || !cellphone || !teacher) {
-        res.status(400).json({error: 'Missing body parameter'});
+): Promise<void> => {
+  if (req.method === 'POST') {
+    const {
+      name,
+      email,
+      cellphone,
+      teacher,
+      courses,
+      available_hours,
+      available_locations,
+    } = req.body;
+
+    if (!teacher) {
+      if (!name || !email || !cellphone) {
+        res.status(400).json({ error: 'Missing body parameter' });
         return;
       }
-
-      const { db } = await connect();
-
-      const response = await db.collection('users').insertOne({
-        name, email, cellphone, teacher,
-      });
-      res.status(200).json(response.ops[0]);
-    } else {
-      res.status(400).json({error: "erro ao processar a requisição"});
+    } else if (teacher) {
+      if (
+        !name ||
+        !email ||
+        !cellphone ||
+        !courses ||
+        !available_hours ||
+        !available_locations
+      ) {
+        res.status(400).json({ error: 'Missing body parameter' });
+        return;
+      }
     }
-}
+
+    const { db } = await connect();
+
+    const response = await db.collection('users').insertOne({
+      name,
+      email,
+      cellphone,
+      teacher,
+      coins: 1,
+      courses: courses || [],
+      available_hours: available_hours || {},
+      available_locations: available_locations || [],
+      reviews: [],
+      appointments: [],
+    });
+    res.status(200).json(response.ops[0]);
+  } else if(req.method === 'GET') {
+
+    const {
+      email,
+    } = req.body;
+    if(!email) {
+      res.status(400).json({ error: 'Missing email!' });
+      return;
+    }
+    const { db } = await connect();
+
+    const response = await db.collection('users').findOne({ email });
+
+    if(!response) {
+      res.status(400).json({ error: 'User with this email not found!' });
+      return;
+    }
+
+    res.status(200).json(response);
+  } else {
+    res.status(400).json({ error: 'erro ao processar a requisição' });
+  }
+};
